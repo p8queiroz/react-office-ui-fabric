78fde56abf668f45d3ed692e680a11a810bc764f
Paulo Queiroz <phqs.phqs@gmail.com>
2019-08-28 19:14:26 -0300

    add test component to solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Main } from '../src/components/Main/Main';
+import Main  from '../src/components/Main/Main';
+import Test  from '../src/components/Test/Test';
+
 
 export const App: React.FunctionComponent = () => {
   return (
     <div>
+      <Test/>
       <Main/>
     </div>
   );
